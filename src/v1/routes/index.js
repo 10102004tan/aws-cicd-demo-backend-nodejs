2a43f39e5640ddeb780f9107e4de528a6f675011
Nguyen Phuong Tan <tannguyen.10102004@gmail.com>
2024-09-21 10:29:55 +0700

    FIX: remove route /

--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -3,10 +3,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/', (req,res)=>{
-    res.status(200).json({message: 'Hello World!'});
-})
-
 router.use('/api/v1/working', (req,res) => {
     res.status(200).json({message: 'it works!'});
 });
