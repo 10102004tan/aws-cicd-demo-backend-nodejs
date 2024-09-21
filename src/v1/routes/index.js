b32ef7725cf0c43ab84236403caed398f5baeb03
Nguyen Phuong Tan <tannguyen.10102004@gmail.com>
2024-09-21 09:51:24 +0700

    ADD: add new route /

--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -3,6 +3,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/', (req,res)=>{
+    res.status(200).json({message: 'Hello World!'});
+})
+
 router.use('/api/v1/working', (req,res) => {
     res.status(200).json({message: 'it works!'});
 });
